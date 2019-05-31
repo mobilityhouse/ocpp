f807cbaffc48c7e115ce7e3f92266b1b82b1a614
Auke Willem Oosterhoff <aukewillem.oosterhoff@mobilityhouse.com>
2019-05-31 12:05:43 +0200

    Bump to 0.1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Change log
+
+## 0.1.1 (2019-05-31)
+
+### Improvements
+
+* [#3](https://github.com/mobilityhouse/ocpp/issues/3) Add CircleCI integration.
+
+### Bug fixes
+
+* [#1](https://github.com/mobilityhouse/ocpp/issues/1) Add JSON schema's to distribution.
+
+## 0.1.0 (2019-05-26)
+
+* Initial release.
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ocpp',
-    version='0.1.0',
+    version='0.1.1',
     description="Python implementation of the Open Charge Point Protocol",
     long_description=long_description,
     long_description_content_type='text/markdown',
