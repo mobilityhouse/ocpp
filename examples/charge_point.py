bc4770c8994bfd97435c90997e28adce67e73a2b
Auke Willem Oosterhoff <aukewillem.oosterhoff@mobilityhouse.com>
2019-06-01 11:30:41 +0200

    Add Python 3.6 support for the examples.

--- examples/charge_point.py
+++ examples/charge_point.py
@@ -1,5 +1,15 @@
 import asyncio
-import websockets
+
+try:
+    import websockets
+except ModuleNotFoundError:
+    print("This example relies on the 'websockets' package.")
+    print("Please install it by running: ")
+    print()
+    print(" $ pip install websockets")
+    import sys
+    sys.exit(1)
+
 
 from ocpp.v16 import call
 from ocpp.v16 import ChargePoint as cp
@@ -31,4 +41,13 @@ async def main():
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        # asyncio.run() is used when running this example with Python 3.7 and
+        # higher.
+        asyncio.run(main())
+    except AttributeError:
+        # For Python 3.6 a bit more code is required to run the main() task on
+        # an event loop.
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
+        loop.close()
