dc349dbac8c921ab3365ad1641c38824a3d62925
me-sosa <milton.sosa@mobilityhouse.com>
2019-10-29 19:12:47 +0100

    adapt examples for ocpp20

---
+++ examples/v20/central_system.py
@@ -0,0 +1,69 @@
+import asyncio
+from datetime import datetime
+
+try:
+    import websockets
+except ModuleNotFoundError:
+    print("This example relies on the 'websockets' package.")
+    print("Please install it by running: ")
+    print()
+    print(" $ pip install websockets")
+    import sys
+    sys.exit(1)
+
+from ocpp.routing import on
+from ocpp.v20 import ChargePoint as cp
+#from ocpp.v16.enums import Action, RegistrationStatus
+from ocpp.v20 import call_result
+
+
+class ChargePoint(cp):
+    @on('BootNotification')
+    def on_boot_notitication(self, charging_station, reason, **kwargs):
+        return call_result.BootNotificationPayload(
+	    current_time=datetime.utcnow().isoformat(),
+	    interval=10,
+	    status='Accepted'
+	)
+
+    @on('Heartbeat')
+    def on_heartbeat(self):
+        print('Got a Heartbeat!')
+        return call_result.HeartbeatPayload(
+                current_time=datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S') + "Z"
+                )
+
+
+async def on_connect(websocket, path):
+    """ For every new charge point that connects, create a ChargePoint instance
+    and start listening for messages.
+
+    """
+    charge_point_id = path.strip('/')
+    cp = ChargePoint(charge_point_id, websocket)
+
+    await cp.start()
+
+
+async def main():
+    server = await websockets.serve(
+        on_connect,
+        '0.0.0.0',
+        9000,
+        subprotocols=['ocpp1.6']
+    )
+
+    await server.wait_closed()
+
+
+if __name__ == '__main__':
+    try:
+        # asyncio.run() is used when running this example with Python 3.7 and
+        # higher.
+        asyncio.run(main())
+    except AttributeError:
+        # For Python 3.6 a bit more code is required to run the main() task on
+        # an event loop.
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
+        loop.close()
