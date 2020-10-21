befe9a10b4e1f6ad809775823a14f0b50ad3f4f0
Johan Falkenström <johan.falkenstrom@gmail.com>
2020-10-21 18:01:46 +0200

    fixxed CI warnings

--- tests/test_charge_point.py
+++ tests/test_charge_point.py
@@ -1,15 +1,14 @@
-import unittest
 from ocpp.v20 import ChargePoint as cp
 
 def test_getters_should_not_be_called_during_routemap_setup():
     class ChargePoint(cp):
         @property
         def foo(self):
-            raise Exception("this will be raised")
+            raise RuntimeException("this will be raised")
 
     try:
         ChargePoint("blah", None)
-    except Exception as e:
+    except RuntimeException as e:
         assert str(e) == "this will be raised"
         assert False, "Getter was called during ChargePoint creation"
     assert True
