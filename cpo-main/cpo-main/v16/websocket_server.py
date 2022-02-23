72029656e0a1d1f04f56bccb4edd384f1c555259
asksimon <simonask42@gmail.com>
2022-02-23 09:00:23 +0100

    CPO project from 2021-11

---
+++ cpo-main/cpo-main/v16/websocket_server.py
@@ -0,0 +1,32 @@
+import websockets
+from functools import partial
+
+
+from http_cpo import CentralSystem
+from cp_cpo import ChargePoint
+
+
+async def on_connect(websocket, path, cpo):
+    """ For every new charge point that connects, create a ChargePoint instance
+    and start listening for messages.
+
+    The ChargePoint is registered at the CSMS.
+
+    """
+    charge_point_id = path.strip("/")
+    cp = ChargePoint(charge_point_id, websocket)
+
+    print(f"Charger {cp.id} connected.")
+
+    # If this handler returns the connection will be destroyed. Therefore we need some
+    # synchronization mechanism that blocks until CSMS wants to close the connection.
+    # An `asyncio.Queue` is used for that.
+    queue = cpo.register_charger(cp)
+    await queue.get()
+
+    
+async def create_websocket_server(cpo: CentralSystem):
+    """ Creating a websocket server that Charge Points can connect to. Using IP, Port and
+    the subprotocol ocpp1.6 (ocpp2.0.1 for the newer version)"""
+    handler = partial(on_connect, cpo=cpo)
+    return await websockets.serve(handler, "0.0.0.0", 9000, subprotocols=["ocpp1.6"])
---
+++ cpo-main/cpo-main/v201/charge_point_v201.py
@@ -0,0 +1,213 @@
+import asyncio
+import logging
+
+import websockets
+from ocpp.routing import on
+from ocpp.v201 import ChargePoint as cp
+from ocpp.v201 import call, call_result
+from ocpp.v201.enums import *
+import datetime
+
+logging.basicConfig(level=logging.INFO)
+
+"""
+This Class is defining the virtual Charge Point.
+All of the functions are hard coded.
+The proper tests are done on proper Charge Point Hardware.
+This is only used to test the OCPP implementation and message routing between HTTP, CSMS and Charge Point.
+Since this is a translated version it has not been properly tested.
+"""
+
+class ChargePoint(cp):
+
+    async def send_authorize(self, id_tag):
+        request = call.AuthorizePayload(
+            id_tag=id_tag
+        )
+        response = await self.call(request)
+
+        if response.id_tag_info['status'] == AuthorizationStatusType.accepted:
+            print("User Authorized")
+
+        elif response.id_tag_info['status'] == AuthorizationStatusType.invalid:
+            print("User Invalid")
+
+        else:
+            print("User Rejected")
+
+
+    async def send_boot_notification(self):
+        request = call.BootNotificationPayload(
+            charging_station={'model':"Drifter",
+            'vendor_name':'Drifter'},
+            reason="PowerUp"
+        )
+
+        response = await self.call(request)
+
+        if response.status == RegistrationStatusType.accepted:
+            print("Connected to central system.")
+            await self.send_heartbeat(response.interval)
+
+    async def send_heartbeat(self, interval):
+        request = call.HeartbeatPayload()
+        while True:
+            await self.call(request)
+            await asyncio.sleep(interval)
+
+    @on(Action.SetVariables)
+    def on_change_config(self, set_variable_data):
+        return call_result.SetVariablesPayload(
+            set_variable_result=None
+        )
+
+    @on(Action.GetVariables)
+    def on_get_config(self, get_variable_data):
+        return call_result.GetVariablesPayload(
+            get_variable_result=None
+        )
+
+    @on(Action.ChangeAvailability)
+    def on_change_availability(self, operational_status):
+        return call_result.ChangeAvailabilityPayload(
+            status=ChangeAvailabilityStatusType.accepted
+        )
+
+    @on(Action.RequestStartTransaction)
+    def on_start_remote(self, id_token, remote_start_id):
+        return call_result.RequestStartTransactionPayload(
+            status=RequestStartStopStatusType.accepted
+        )
+
+    @on(Action.RequestStopTransaction)
+    def on_stop_remote(self, transaction_id):
+        return call_result.RequestStopTransactionPayload(
+            status=RequestStartStopStatusType.accepted
+        )
+
+    @on(Action.ReserveNow)
+    def on_reserve_now(self, id, expiry_date, id_token, connector_type):
+        return call_result.ReserveNowPayload(
+            status=ReserveNowStatusType.accepted
+        )
+
+    @on(Action.CancelReservation)
+    def on_cancel_reservation(self, reservation_id):
+        return call_result.CancelReservationPayload(
+            status=CancelReservationStatusType.accepted
+        )
+
+    @on(Action.GetCompositeSchedule)
+    def on_cancel_reservation(self, connector_id, duration, charging_rate_unit):
+        return call_result.GetCompositeSchedulePayload(
+            status=GenericStatusType.accepted
+        )
+
+    @on(Action.GetLocalListVersion)
+    def on_get_local_list(self):
+        return call_result.GetLocalListVersionPayload(
+            version_number=1
+        )
+
+    @on(Action.SendLocalList)
+    def on_send_local_list(self, list_version, local_authorization_list, update_type):
+        return call_result.SendLocalListPayload(
+            status=SendLocalListStatusType.accepted
+        )
+
+    @on(Action.ClearChargingProfile)
+    def on_clear_charging_profile(self, charging_profile_id):
+        return call_result.ClearChargingProfilePayload(
+            status=ClearChargingProfileStatusType.accepted
+        )
+
+
+    @on(Action.ClearCache)
+    def on_clear_cache(self):
+        return call_result.ClearCachePayload(
+            status=ClearCacheStatusType.accepted
+        )
+
+    async def send_data_transfer(self, vendor_id, message_id, data):
+        request = call.DataTransferPayload(
+            vendor_id=vendor_id,
+            message_id=message_id,
+            data=data
+        )
+        response = await self.call(request)
+
+        if response.status == DataTransferStatusType.accepted:
+            print("Transfer Accepted")
+
+        elif response.status == DataTransferStatusType.rejected:
+            print("Transfer Rejected")
+
+        else:
+            print("User Rejected")    
+
+    async def send_meter_values(self, evse_id, meter_value):
+        request = call.MeterValuesPayload(
+            evse_id=evse_id,
+            meter_value=meter_value
+        )
+        response = await self.call(request)  
+
+    @on(Action.Reset)
+    def on_reset(self, type):
+        return call_result.ResetPayload(
+            status=ResetStatusType.accepted
+        )
+    
+    async def transaction_event(self, event_type, timestamp, trigger_reason, seq_no, transaction_id, charge_state, evse, id_token, **kwargs):
+        request = call.TransactionEventPayload(
+            event_type="Started",
+            timestamp=datetime.utcnow().isoformat(),
+            trigger_reason="Authorized",
+            seq_no=1,
+            
+            transaction_info=
+                {"transaction_id":0,
+                "charging_state":"EVConnected"},
+            evse=evse,
+            id_token={
+                'id_token':"123abc",
+                'type': "Local"}
+        )
+        response = await self.call(request)
+
+
+    @on(Action.TriggerMessage)
+    def unlock_connector(self, requested_message):
+        return call_result.TriggerMessagePayload(
+            status = TriggerMessageStatusType.accepted
+        )
+
+
+    @on(Action.UnlockConnector)
+    def unlock_connector(self, evse_id, connector_id):
+        return call_result.UnlockConnectorPayload(
+            status = UnlockStatusType.unlocked
+        )
+
+
+async def main():
+    async with websockets.connect(
+        'ws://localhost:9000/CP',
+        subprotocols=["ocpp1.6"]
+    ) as ws:
+
+        cp = ChargePoint('CP', ws)
+        await asyncio.gather(cp.start(), cp.send_boot_notification())
+
+if __name__ == '__main__':
+    try:
+        # asyncio.run() is used when
+        #  running this example with Python 3.7 and
+        # higher.
+        asyncio.run(main())
+    except AttributeError:
+        # For Python 3.6 a bit more code is required to run the main() task on
+        # an event loop.
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
+        loop.close()
