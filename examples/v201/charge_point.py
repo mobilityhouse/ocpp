d2471f7877f2883eccc449ad376e2634dc36b76f
laysa.uchoa <laysa.uchoa@aiven.io>
2022-05-02 21:07:53 +0200

    Drop Python 3.6 in examples

--- examples/v201/charge_point.py
+++ examples/v201/charge_point.py
@@ -52,14 +52,6 @@ async def main():
                              charge_point.send_boot_notification())
 
 
-if __name__ == '__main__':
-    try:
-        # asyncio.run() is used when running this example with Python 3.7 and
-        # higher.
-        asyncio.run(main())
-    except AttributeError:
-        # For Python 3.6 a bit more code is required to run the main() task on
-        # an event loop.
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main())
-        loop.close()
+if __name__ == "__main__":
+    # asyncio.run() is used when running this example with Python >= 3.7v
+    asyncio.run(main())
