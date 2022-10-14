bc0af070357b16e02c63a5317020d2364afe2961
Nick Bout <nick-b@live.nl>
2022-10-14 13:45:30 +0200

    Added missing ' (single quote) to error message

--- ocpp/messages.py
+++ ocpp/messages.py
@@ -210,7 +210,7 @@ def validate_payload(message, ocpp_version):
                 details={"cause": e.message}) from e
         else:
             raise FormatViolationError(
-                details={"cause": f"Payload '{message.payload} for action "
+                details={"cause": f"Payload '{message.payload}' for action "
                          f"'{message.action}' is not valid: {e}"})
 
 
