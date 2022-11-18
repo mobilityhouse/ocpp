2a0a77d65ec514fe8e9629024ccc7bbe1251261e
Patrick Roelke <proelke@gmail.com>
2022-11-18 01:54:39 +0100

    fix: remove misplaced code

--- ocpp/__init__.py
+++
@@ -1,6 +0,0 @@
-from ocpp.charge_point import ChargePoint as cp
-from ocpp.messages import OCPPVersion, SchemaValidator
-
-
-class ChargePoint(cp):
-    _schema_validator = SchemaValidator(ocpp_version=OCPPVersion.v201)
