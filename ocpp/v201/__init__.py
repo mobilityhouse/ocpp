4211f9a1de065f967e81fa4eb3bf155533fcfe74
Patrick Roelke <proelke@gmail.com>
2022-11-22 22:18:33 +0100

    refactor: improve schema_path creation

--- ocpp/v201/__init__.py
+++ ocpp/v201/__init__.py
@@ -3,7 +3,7 @@ import pathlib
 from ocpp.charge_point import ChargePoint as cp
 from ocpp.messages import SchemaValidator
 
-schema_path = str(pathlib.Path(__file__).parent.resolve()) + "/schemas"
+schema_path = str(pathlib.Path(__file__).parent.joinpath("schemas").resolve())
 validator = SchemaValidator(path_to_schemas=schema_path)
 
 
