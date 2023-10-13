98316eb1b14a68897a4548337154479408674c4a
Jared Newell <jared.newell@mobilityhouse.com>
2023-10-13 09:53:20 +0200

    completion_of_referenced_components_variables_instance_201

--- ocpp/v201/enums.py
+++ ocpp/v201/enums.py
@@ -1431,32 +1431,445 @@ class ComponentName(str, Enum):
     vehicle_id_sensor = "VehicleIdSensor"
 
 
-class VariableName(str, Enum):
+class GenericVariableName(str, Enum):
+    """
+    Variable names where the component type is non-specific
+    """
+    ac_current = "ACCurrent"
+    active = "Active"
+    ac_voltage = "ACVoltage"
+    allow_reset = "AllowReset"
+    angle = "Angle"
+    attempts = "Attempts"
+    available = "Available"
+    certificate = "Certificate"
+    color = "Color"
+    complete = "Complete"
+    connected_time = "ConnectedTime"
+    count = "Count"
+    current_imbalance = "CurrentImbalance"
+    data_text = "DataText"
+    date_time = "DateTime"
+    dc_current = "DCCurrent"
+    dc_voltage = "DCVoltage"
+    ec_variant = "ECVariant"
+    enabled = "Enabled"
+    energy = "Energy"
+    entries = "Entries"
+    fallback = "Fallback"
+    fan_speed = "FanSpeed"
+    firmware_version = "FirmwareVersion"
+    force = "Force"
+    formats = "Formats"
+    frequency = "Frequency"
+    fuse_rating = "FuseRating"
+    height = "Height"
+    humidity = "Humidity"
+    hysteresis = "Hysteresis"
+    iccid = "ICCID"
+    impedance = "Impedance"
+    imsi = "IMSI"
+    interval = "Interval"
+    length = "Length"
+    light = "Light"
+    manufacturer = "Manufacturer"
+    message = "Message"
+    minimum_status_duration = "MinimumStatusDuration"
+    mode = "Mode"
+    model = "Model"
+    network_address = "NetworkAddress"
+    operated = "Operated"
+    operating_times = "OperatingTimes"
+    overload = "Overload"
+    percent = "Percent"
+    phase_rotation = "PhaseRotation"
+    post_charging_time = "PostChargingTime"
+    power = "Power"
+    problem = "Problem"
+    protecting = "Protecting"
+    serial_number = "SerialNumber"
+    signal_strength = "SignalStrength"
+    state = "State"
+    state_of_charge = "StateOfCharge"
+    storage = "Storage"
+    supply_phases = "SupplyPhases"
+    suspending = "Suspending"
+    suspension = "Suspension"
+    temperature = "Temperature"
+    time = "Time"
+    timeout = "Timeout"
+    tries = "Tries"
+    tripped = "Tripped"
+    vendor_name = "VendorName"
+    version_date = "VersionDate"
+    version_number = "VersionNumber"
+    voltage_imbalance = "VoltageImbalance"
 
-    # 2.1.General
 
-    # 2.2. Security related
+class AlignedDataCtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is AlignedDataCtrlr
+    """
+    available = "Available"
+    enabled = "Enabled"
+    interval = "Interval"
+    measurands = "Measurands"
+    send_during_idle = "SendDuringIdle"
+    sign_readings = "SignReadings"
+    tx_ended_interval = "TxEndedInterval"
+    tx_ended_measurands = "TxEndedMeasurands"
+    available = "Available"
+    enabled = "Enabled"
+    life_time = "LifeTime"
+    policy = "Policy"
+    storage = "Storage"
+    disable_post_authorize = "DisablePostAuthorize"
 
-    # 2.3. Authorization related
 
-    # 2.4. Authorization Cache related
+class AuthCtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is AuthCtrlr
+    """
+    additional_info_items_per_message = "AdditionalInfoItemsPerMessage"
+    authorize_remote_start = "AuthorizeRemoteStart"
+    enabled = "Enabled"
+    local_authorize_offline = "LocalAuthorizeOffline"
+    local_pre_authorize = "LocalPreAuthorize"
+    master_pass_group_id = "MasterPassGroupId"
+    offline_tx_for_unknown_id_enabled = "OfflineTxForUnknownIdEnabled"
+    disable_remote_authorization = "DisableRemoteAuthorization"
 
-    # 2.5. Local Authorization List Management related
 
-    # 2.6. Transaction related
+class CHAdeMOCtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is CHAdeMOCtrlr
+    """
+    selftest_active = "SelftestActive"
+    chademo_protocol_number = "CHAdeMOProtocolNumber"
+    vehicle_status = "VehicleStatus"
+    dynamic_control = "DynamicControl"
+    high_current_control = "HighCurrentControl"
+    high_voltage_control = "HighVoltageControl"
+    auto_manufacturer_code = "AutoManufacturerCode"
 
-    # 2.7.Metering related
 
-    # 2.8. Reservation related
+class ChargingStationVariableName(str, Enum):
+    """
+    Variable names where the component type is ChargingStation
+    """
+    allow_new_sessions_pending_firmware_update = "AllowNewSessionsPendingFirmwareUpdate"
+    availability_state = "AvailabilityState"
+    available = "Available"
+    model = "Model"
+    supply_phases = "SupplyPhases"
+    vendor_name = "VendorName"
 
-    # 2.9. Smart Charging related
 
-    # 2.10. Tariff & Cost related
+class ClockCtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is ClockCtrlr
+    """
+    date_time = "DateTime"
+    next_time_offset_transition_date_time = "NextTimeOffsetTransitionDateTime"
+    ntp_server_uri = "NtpServerUri"
+    ntp_source = "NtpSource"
+    time_adjustment_reporting_threshold = "TimeAdjustmentReportingThreshold"
+    time_offset = "TimeOffset"
+    time_source = "TimeSource"
+    time_zone = "TimeZone"
 
-    # 2.11. Diagnostics related
 
-    # 2.12. Display Message related
+class ConnectedEVVariableName(str, Enum):
+    """
+    Variable names where the component type is ConnectedEV
+    """
+    protocol_agreed = "ProtocolAgreed"
+    protocol_supported_by_ev = "ProtocolSupportedByEV"
+    vehicle_id = "VehicleID"
 
-    # 2.13. Charging Infrastructure related
 
-    # 2.14. ISO 15118 Related
+class ConnectorVariableName(str, Enum):
+    """
+    Variable names where the component type is Connector
+    """
+    availability_state = "AvailabilityState"
+    available = "Available"
+    charge_protocol = "ChargeProtocol"
+    connector_type = "ConnectorType"
+    supply_phases = "SupplyPhases"
+    max_msg_elements = "MaxMsgElements"
+
+
+class CPPWMControllerVariableName(str, Enum):
+    """
+    Variable names where the component type is CPPWMController
+    """
+    state = "State"
+
+
+class CustomizationCtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is CustomizationCtrlr
+    """
+    custom_implementation_enabled = "CustomImplementationEnabled"
+
+
+class DeviceDataCtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is DeviceDataCtrlr
+    """
+    bytes_per_message = "BytesPerMessage"
+    configuration_value_size = "ConfigurationValueSize"
+    items_per_message = "ItemsPerMessage"
+
+
+class DeviceDataCtrlrInstanceName(str, Enum):
+    """
+    Instance names where the component type is DeviceDataCtrlr
+    """
+    get_report = "GetReport"
+    get_variables = "GetVariables"
+    set_variables = "SetVariables"
+    reporting_value_size = "ReportingValueSize"
+    value_size = "ValueSize"
+
+
+class DisplayMessageCtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is DisplayMessageCtrlr
+    """
+    available = "Available"
+    display_messages = "DisplayMessages"
+    enabled = "Enabled"
+    personal_message_size = "PersonalMessageSize"
+    supported_formats = "SupportedFormats"
+    supported_priorities = "SupportedPriorities"
+
+
+class DistributionPanelVariableName(str, Enum):
+    """
+    Variable names where the component type is DistributionPanel
+    """
+    charging_station = "ChargingStation"
+    distribution_panel = "DistributionPanel"
+    fuse = "Fuse"
+
+
+class EVSEVariableName(str, Enum):
+    """
+    Variable names where the component type is EVSE
+    """
+    allow_reset = "AllowReset"
+    availability_state = "AvailabilityState"
+    available = "Available"
+    evse_id = "EvseId"
+    power = "Power"
+    supply_phases = "SupplyPhases"
+    iso15118_evse_id = "ISO15118EvseId"
+
+
+class FiscalMeteringVariableName(str, Enum):
+    """
+    Variable names where the component type is FiscalMetering
+    """
+    energy_export = "EnergyExport"
+    energy_export_register = "EnergyExportRegister"
+    energy_import = "EnergyImport"
+    energy_import_register = "EnergyImportRegister"
+
+
+class ISO15118CtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is ISO15118Ctrlr
+    """
+    central_contract_validation_allowed = 'CentralContractValidationAllowed'
+    contract_validation_offline = 'ContractValidationOffline'
+    secc_id = 'SeccId'
+    max_schedule_entries = 'MaxScheduleEntries'
+    requested_energy_transfer_mode = 'RequestedEnergyTransferMode'
+    request_metering_receipt = 'RequestMeteringReceipt'
+    country_name = 'CountryName'
+    organization_name = 'OrganizationName'
+    pnc_enabled = 'PnCEnabled'
+    v2g_certificate_installation_enabled = 'V2GCertificateInstallationEnabled'
+    contract_certificate_installation_enabled = 'ContractCertificateInstallationEnabled'
+
+
+class LocalAuthListCtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is LocalAuthListCtrlr
+    """
+    available = "Available"
+    bytes_per_message = "BytesPerMessage"
+    enabled = "Enabled"
+    entries = "Entries"
+    items_per_message = "ItemsPerMessage"
+    storage = "Storage"
+    disable_post_authorize = "DisablePostAuthorize"
+
+
+class LocalEnergyStorageVariableName(str, Enum):
+    """
+    Variable names where the component type is LocalEnergyStorage
+    """
+    capacity = "Capacity"
+
+
+class MonitoringCtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is MonitoringCtrlr
+    """
+    available = "Available"
+    bytes_per_message = "BytesPerMessage"
+    enabled = "Enabled"
+    items_per_message = "ItemsPerMessage"
+    OfflineQueuingSeverity = "OfflineQueuingSeverity"
+    MonitoringBase = "MonitoringBase"
+    MonitoringLevel = "MonitoringLevel"
+    ActiveMonitoringBase = "ActiveMonitoringBase"
+    ActiveMonitoringLevel = "ActiveMonitoringLevel"
+
+
+class MonitoringCtrlrInstanceName(str, Enum):
+    """
+    Instance names where the component type is MonitoringCtrlr
+    """
+    clear_variable_monitoring = "ClearVariableMonitoring"
+    set_variable_monitoring = "SetVariableMonitoring"
+
+
+class OCPPCommCtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is OCPPCommCtrlr
+    """
+    active_network_profile = "ActiveNetworkProfile"
+    file_transfer_protocols = "FileTransferProtocols"
+    heartbeat_interval = "HeartbeatInterval"
+    message_timeout = "MessageTimeout"
+    message_attempt_interval = "MessageAttemptInterval"
+    message_attempts = "MessageAttempts"
+    network_configuration_priority = "NetworkConfigurationPriority"
+    network_profile_connection_attempts = "NetworkProfileConnectionAttempts"
+    offline_threshold = "OfflineThreshold"
+    public_key_with_signed_meter_value = "PublicKeyWithSignedMeterValue"
+    queue_all_messages = "QueueAllMessages"
+    reset_retries = "ResetRetries"
+    retry_back_off_random_range = "RetryBackOffRandomRange"
+    retry_back_off_repeat_times = "RetryBackOffRepeatTimes"
+    retry_back_off_wait_minimum = "RetryBackOffWaitMinimum"
+    unlock_on_ev_side_disconnect = "UnlockOnEVSideDisconnect"
+    web_socket_ping_interval = "WebSocketPingInterval"
+    field_length = "FieldLength"
+
+
+class OCPPCommCtrlrInstanceName(str, Enum):
+    """
+    Instance names where the component type is OCPPCommCtrlr
+    """
+    default = "Default"
+    transaction_event = "TransactionEvent"
+
+
+class ReservationCtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is ReservationCtrlr
+    """
+    available = "Available"
+    enabled = "Enabled"
+    non_evse_specific = "NonEvseSpecific"
+
+
+class SampledDataCtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is SampledDataCtrlr
+    """
+    available = "Available"
+    enabled = "Enabled"
+    sign_readings = "SignReadings"
+    tx_ended_interval = "TxEndedInterval"
+    tx_ended_measurands = "TxEndedMeasurands"
+    tx_started_measurands = "TxStartedMeasurands"
+    tx_updated_interval = "TxUpdatedInterval"
+    tx_updated_measurands = "TxUpdatedMeasurands"
+    register_values_without_phases = "RegisterValuesWithoutPhases"
+
+
+class SecurityCtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is SampledDataCtrlr
+    """
+    additional_root_certificate_check = "AdditionalRootCertificateCheck"
+    basic_auth_password = "BasicAuthPassword"
+    certificate_entries = "CertificateEntries"
+    cert_signing_repeat_times = "CertSigningRepeatTimes"
+    cert_signing_wait_minimum = "CertSigningWaitMinimum"
+    identity = "Identity"
+    max_certificate_chain_size = "MaxCertificateChainSize"
+    organization_name = "OrganizationName"
+    security_profile = "SecurityProfile"
+
+
+class SmartChargingCtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is SmartChargingCtrlr
+    """
+    ACPhaseSwitchingSupported = "ACPhaseSwitchingSupported"
+    Available = "Available"
+    Enabled = "Enabled"
+    Entries = "Entries"
+    ExternalControlSignalsEnabled = "ExternalControlSignalsEnabled"
+    LimitChangeSignificance = "LimitChangeSignificance"
+    NotifyChargingLimitWithSchedules = "NotifyChargingLimitWithSchedules"
+    PeriodsPerSchedule = "PeriodsPerSchedule"
+    Phases3to1 = "Phases3to1"
+    ProfileStackLevel = "ProfileStackLevel"
+    RateUnit = "RateUnit"
+
+class SmartChargingCtrlrInstanceName(str, Enum):
+    """
+    Instance names where the component type is SmartChargingCtrlr
+    """
+    charging_profiles = "ChargingProfiles"
+
+
+class TariffCostCtrlrVariableName(str, Enum):
+    """
+    Variable names where the component type is TariffCostCtrlr
+    """
+    available = "Available"
+    currency = "Currency"
+    enabled = "Enabled"
+    tariff_fallback_message = "TariffFallbackMessage"
+    total_cost_fallback_message = "TotalCostFallbackMessage"
+
+
+class TariffCostCtrlrInstanceName(str, Enum):
+    """
+    Instance names where the component type is TariffCostCtrlr
+    """
+    tariff = "Tariff"
+    cost = "Cost"
+
+
+class TokenReaderVariableName(str, Enum):
+    """
+    Variable names where the component type is TokenReader
+    """
+    token = "Token"
+    token_type = "TokenType"
+
+
+class TxCtrlrVariableName(str, Enum):
+    """
+    Instance names where the component type is TxCtrlr
+    """
+    charging_time = "ChargingTime"
+    ev_connection_time_out = "EVConnectionTimeOut"
+    max_energy_on_invalid_id = "MaxEnergyOnInvalidId"
+    stop_tx_on_ev_side_disconnect = "StopTxOnEVSideDisconnect"
+    stop_tx_on_invalid_id = "StopTxOnInvalidId"
+    tx_before_accepted_enabled = "TxBeforeAcceptedEnabled"
+    tx_start_point = "TxStartPoint"
+    tx_stop_point = "TxStopPoint"
+
+
