256d7a32b196477b309efada133f9fcbac22f40a
drc38 <20024196+drc38@users.noreply.github.com>
2023-10-20 13:10:05 +1300

    create routemap in test

--- tests/v16/test_v16_charge_point.py
+++ tests/v16/test_v16_charge_point.py
@@ -115,6 +115,19 @@ async def test_route_message_not_supported(base_central_system, notsupported_cal
     not supported by OCPP version.
 
     """
+    @on(Action.BootNotification)
+    def on_boot_notification(**kwargs):  # noqa
+        assert kwargs["firmware_version"] == "#1:3.4.0-2990#N:217H;1.0-223"
+
+        return call_result.BootNotificationPayload(
+            current_time="2018-05-29T17:37:05.495259",
+            interval=350,
+            # 'Yolo' is not a valid value for for field status.
+            status="Yolo",
+        )
+
+    setattr(base_central_system, "on_boot_notification", on_boot_notification)
+    base_central_system.route_map = create_route_map(base_central_system)
 
     await base_central_system.route_message(notsupported_call)
     base_central_system._connection.send.assert_called_once_with(
