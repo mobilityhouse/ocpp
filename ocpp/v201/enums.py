88e20060aa9f4e741fe894cb6ec4d8f1c23fc91f
Jared-Newell-Mobility <119603687+Jared-Newell-Mobility@users.noreply.github.com>
2023-12-06 13:18:49 +0100

    OCPP-2.0.1-add-additional-security-events-in-v1.3 (#517)

--- ocpp/v201/enums.py
+++ ocpp/v201/enums.py
@@ -1330,7 +1330,9 @@ class StatusInfoReasonType(str, Enum):
 
 
 class SecurityEventType(str, Enum):
-    """Security Events as listed in Appendices (Appendix 1. Security Events)"""
+    """
+    Security Events as listed in Appendices (Appendix 1. Security Events) v1.3
+    """
 
     firmware_updated = "FirmwareUpdated"
     failed_to_authenticate_at_csms = "FailedToAuthenticateAtCsms"
@@ -1350,3 +1352,5 @@ class SecurityEventType(str, Enum):
     invalid_charging_station_certificate = "InvalidChargingStationCertificate"
     invalid_tls_version = "InvalidTLSVersion"
     invalid_tls_cipher_suite = "InvalidTLSCipherSuite"
+    maintenance_login_accepted = "MaintenanceLoginAccepted"
+    maintenance_login_failed = "MaintenanceLoginFailed"
