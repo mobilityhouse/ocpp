e13e8e8cccaaa6caaf73eb24ffb5163425da7ca4
Jared Newell <jared.newell@mobilityhouse.com>
2024-02-14 15:40:45 +0100

    used normalised version - 1.0.0rc1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2024, Auke Willem Oosterhoff"
 author = "Auke Willem Oosterhoff"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0-rc.1"
+release = "1.0.0rc1"
 
 
 # -- General configuration ---------------------------------------------------
