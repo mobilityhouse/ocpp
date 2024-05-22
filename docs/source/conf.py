765799e4acbdb609bcf8643b1562349bede755de
Auke Willem Oosterhoff <1565144+OrangeTux@users.noreply.github.com>
2024-05-22 11:37:48 +0200

    Bump to 2.0.0-rc.0 (#638)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2024, Auke Willem Oosterhoff"
 author = "Auke Willem Oosterhoff"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "2.0.0-rc.0"
 
 
 # -- General configuration ---------------------------------------------------
