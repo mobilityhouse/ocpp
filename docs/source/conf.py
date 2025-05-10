ac1460e75dd9494fda9b4a3b61eb5c9a0c950b5a
Mohit Jain <jainmohit2001@gmail.com>
2025-05-10 18:24:22 +0530

    Add Sphinx configuration and initial documentation for OCPP - Added installation, client and server usage and debugging docs

---
+++ docs/source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'ocpp'
+copyright = '2025, Mohit Jain'
+author = 'Mohit Jain'
+release = '3.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_theme_options = {
+    'collapse_navigation': False,
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. ocpp documentation master file, created by
+   sphinx-quickstart on Sun May  4 14:28:47 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+ocpp documentation
+==================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   installation
+   usage/index
+   debugging
